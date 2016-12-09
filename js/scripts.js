93940e71e9995c82fc4a73e61387c872633572f0
connorbacon9 <connorbacon9@yahoo.com>
2016-12-08 18:38:17 -0800

    weather_App

---
+++ js/scripts.js
@@ -0,0 +1,99 @@
+// Spokane
+
+$.simpleWeather({
+    location: '99205',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.spokane .temp').text(weather.temp);
+      $('.spokane .city').text(weather.city);
+     
+      $('.spokane title').text(weather.title);
+      
+      //  If this condition, show this icon
+      if ( 26 >= weather.code && 30 <= weather.code ) {
+          $('.spokane .wi').addClass('wi-day-cloudy');
+      }
+        
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
+// Phoenix
+
+$.simpleWeather({
+    location: '85034',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.phoenix .temp').text(weather.temp);
+      $('.phoenix .city').text(weather.city);
+      
+      $('.phoenix title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
+// Los Angeles
+
+$.simpleWeather({
+    location: '90045',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.la .temp').text(weather.temp);
+      $('.la .city').text(weather.city);
+   
+      $('.la title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
+// Seattle
+
+$.simpleWeather({
+    location: '98101',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.seattle .temp').text(weather.temp);
+      $('.seattle .city').text(weather.city);
+    
+      $('.seattle title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
+//GEOLOCATION
+
